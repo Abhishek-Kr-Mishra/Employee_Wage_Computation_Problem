7dd1c603d165beb55bf7105019856981976ccce5
abh224272 <abh224272@gmail.com>
2020-11-16 15:22:13 +0530

    Adding: Calculate Wages till a condition e.g(For 20 Days Or 1000 Hours)

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,32 +4,29 @@
 
 IS_FULL_TIME=1
 IS_PART_TIME=2
-perDaySalary=0
-monthSalary=0
 WORK_DAY_IN_A_MONTH=20
 EMP_RATE_PER_HOUR=20
-totalSalary=0
+MAX_HOURS_IN_MONTH=1000
+totalWorkingDays=0
+totalWorkHours=0
 
-	for (( day=1; day<=$WORK_DAY_IN_A_MONTH; day++))
+	while(( $totalWorkHours < $MAX_HOURS_IN_MONTH && $totalWorkingDays < $WORK_DAY_IN_A_MONTH ))
 	do
-	attendanceValue=$(( RANDOM%3 ))
-	case $attendanceValue in
-		1) echo "Employee is Full Time"
-			 workHour=8
-			HoursCondition=$(( $HoursCondition+8 ))
-			((DaysCondition++))
-			     ;;
-		2) echo "Employee is Part Time"
-			 workHour=4
-			HoursCondition=$(( $HoursCondition+4 ))
-			((DaysCondition++))
-			     ;;
-		*) workHour=0
-	esac
+		((totalWorkingDays++))
+		echo "Total Working Day : $totalWorkingDays"
+		attendanceValue=$(( RANDOM%3 ))
+		case $attendanceValue in
+			1) echo "Employee is Full Time"
+			 	workHour=8
+			     		;;
+			2) echo "Employee is Part Time"
+			 	workHour=4
+			     	;;
+			*) workHour=0
+		esac
+			totalWorkHours=$(( $totalWorkHours+$workHour ))
+			echo -e "Total Employee Hours: $totalWorkHours \n"
+		done
 
-	perDaySalary=$(( $EMP_RATE_PER_HOUR*$workHour ))
-	echo "Day : $day          Salary : $perDaySalary"
-	totalSalary=$(( $totalSalary+$perDaySalary ))
-	done
-
-	echo "Total Salary is $totalSalary"
+	totalSalary=$(( $totalWorkHours*$EMP_RATE_PER_HOUR ))
+	echo "Total Salary is $totalSalary "

f7d9d3c6ed6a201ef014ec63373b44f65ab01631
abh224272 <abh224272@gmail.com>
2020-11-16 12:20:48 +0530

    Adding: UC1 to check the Employee's Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,11 @@
 
 	echo "Welcome to Employee Wage Computation Program "
 
+	attendanceValue=$(( RANDOM%2 ))
+	if (( attendanceValue==1 ))
+	then
+		echo "Employee is Present "
+	else
+		echo "Employee is Absent"
+	fi
+

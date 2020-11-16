9677701d79d1aa4453a3d6ae74d8cf7ab10b10f9
abh224272 <abh224272@gmail.com>
2020-11-16 12:58:39 +0530

    Adding: Part-Time and Full-Time salary criteria

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,18 +2,24 @@
 
 	echo "Welcome to Employee Wage Computation Program "
 
+IS_FULL_TIME=1
+IS_PART_TIME=2
 salary=0
 EMP_RATE_PER_HOUR=20
 
-	attendanceValue=$(( RANDOM%2 ))
-	if (( attendanceValue==1 ))
+	attendanceValue=$(( RANDOM%3 ))
+	echo "$attendanceValue"
+	if (( $attendanceValue==$IS_FULL_TIME ))
 	then
-		echo "Employee is Present "
+		echo "Employee is Full Time "
 		workHour=8
-		salary=$(( $EMP_RATE_PER_HOUR*$workHour ))
+	elif (( $attendanceValue==$IS_PART_TIME ))
+	then
+		echo "Employee is Part Time"
+		workHour=4
 	else
-		echo "Employee is Absent"
-		salary=0
+		workHour=0
 	fi
 
+	salary=$(( $EMP_RATE_PER_HOUR*$workHour ))
 	echo "Salary =  $salary"

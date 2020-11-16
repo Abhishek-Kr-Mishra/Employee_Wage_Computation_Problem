155bec71cc6925e41ea01c1cf8b03837913807d3
abh224272 <abh224272@gmail.com>
2020-11-16 12:32:59 +0530

    Adding: Calcultaion of salary on the basis of Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,11 +2,18 @@
 
 	echo "Welcome to Employee Wage Computation Program "
 
+salary=0
+EMP_RATE_PER_HOUR=20
+
 	attendanceValue=$(( RANDOM%2 ))
 	if (( attendanceValue==1 ))
 	then
 		echo "Employee is Present "
+		workHour=8
+		salary=$(( $EMP_RATE_PER_HOUR*$workHour ))
 	else
 		echo "Employee is Absent"
+		salary=0
 	fi
 
+	echo "Salary =  $salary"

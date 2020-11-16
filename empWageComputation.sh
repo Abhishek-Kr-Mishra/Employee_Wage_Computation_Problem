34bd2f94c2078bd7498cfe011d6716d88266cbb6
abh224272 <abh224272@gmail.com>
2020-11-16 14:15:33 +0530

    Refactor: Rename Commit message.... Calculating wages for one month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,22 +8,28 @@ perDaySalary=0
 monthSalary=0
 WORK_DAY_IN_A_MONTH=20
 EMP_RATE_PER_HOUR=20
+totalSalary=0
 
+	for (( day=1; day<=$WORK_DAY_IN_A_MONTH; day++))
+	do
 	attendanceValue=$(( RANDOM%3 ))
-	echo "Random Number: $attendanceValue"
 	case $attendanceValue in
 		1) echo "Employee is Full Time"
 			 workHour=8
+			HoursCondition=$(( $HoursCondition+8 ))
+			((DaysCondition++))
 			     ;;
 		2) echo "Employee is Part Time"
 			 workHour=4
+			HoursCondition=$(( $HoursCondition+4 ))
+			((DaysCondition++))
 			     ;;
 		*) workHour=0
 	esac
 
-	echo "Work Hours : $workHour"
 	perDaySalary=$(( $EMP_RATE_PER_HOUR*$workHour ))
-	monthSalary=$(( $perDaySalary*$EMP_RATE_PER_HOUR ))
-	echo "Salary =  $perDaySalary"
-	echo "Salary of a Month = $monthSalary"
+	echo "Day : $day          Salary : $perDaySalary"
+	totalSalary=$(( $totalSalary+$perDaySalary ))
+	done
 
+	echo "Total Salary is $totalSalary"
